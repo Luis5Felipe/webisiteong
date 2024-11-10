5c301589d73c6984043662d0bbd6984fb89fda4a
Luis5Felipe <luisfelipedos29@gmail.com>
2024-11-10 12:37:15 -0300

    Update pagina home

--- Webisite/views.py
+++ Webisite/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from .models import MidiaEventos
 
-
-
 def home_view(request):
-    eventos = MidiaEventos.objects.order_by('-data_evento')
-    return render(request,'_layouts/home.html', {'eventos': eventos})
+    # Aqui, estamos pegando os 3 primeiros eventos como exemplo
+    evento1 = MidiaEventos.objects.all()[0]
+    evento2 = MidiaEventos.objects.all()[1]  # Segundo evento
+    evento3 = MidiaEventos.objects.all()[2]  # Terceiro evento
+
+    return render(request, '_layouts/home.html', {
+        'evento1': evento1,
+        'evento2': evento2,
+        'evento3': evento3
+    })
 
 # def eventos_list(request):
 #     # Recupera todos os objetos de MidiaEventos
