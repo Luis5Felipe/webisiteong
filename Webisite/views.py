1174348cc1020edabcb7746a44b509f1dd85c9a0
Luis5Felipe <luisfelipedos29@gmail.com>
2024-11-11 10:38:24 -0300

    Tratamentto de erro

--- Webisite/views.py
+++ Webisite/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import MidiaEventos
 from .models import Voluntario
+from .models import Paciente
+from datetime import datetime , date
+from django.core.exceptions import ValidationError
+from validate_docbr import CPF
+from django.contrib import messages 
 
 def home_view(request):
     eventos = MidiaEventos.objects.all().order_by('data_evento')[:3]
@@ -45,15 +50,58 @@ def voluntario_view(request):
     
     return render(request, '_layouts/volunario.html')
 
-def consulta_view(request): 
-#     if request.method == 'POST':
-#         nome = request.POST.get('nome_Voluntario',None)
-#         cpf = request.POST.get('cpf_Voluntario', None)
-#         data = request.POST.get('data_Nascimento', None)
-#         email = request.POST.get('email', None)
-#         telefone = request.POST.get('telefone', None)
-#         genero = request.POST.get('genero', None)
-     return render(request,'_layouts/consulta.html', )
+
+def consulta_view(request):
+    if request.method == 'POST':
+        nome_res = request.POST.get('nome_responsavel', None)
+        nome = request.POST.get('nome_paciente', None)
+        cpf = request.POST.get('cpf_responsavel', None)
+        data = request.POST.get('data_Nascimento', None)
+        email = request.POST.get('email', None)
+        telefone = request.POST.get('telefone', None)
+        endereco = request.POST.get('endereco', None)
+        genero = request.POST.get('genero', None)
+
+        try:
+            # Convertendo a data de nascimento
+            data_nascimento = datetime.strptime(data, '%Y-%m-%d').date() if data else None
+
+            if data_nascimento:
+                today = date.today()
+                idade_calculada = today.year - data_nascimento.year - ((today.month, today.day) < (data_nascimento.month, data_nascimento.day))
+                
+                if idade_calculada < 0:
+                    raise ValidationError("Idade inválida calculada para o voluntário.")
+            else:
+                idade_calculada = None 
+
+            # Validando o CPF
+            cpf_validator = CPF()
+            if not cpf_validator.validate(cpf):
+                raise ValidationError("CPF do voluntário inválido.")
+
+            # Criando a instância do modelo Paciente
+            paciente_instance = Paciente(
+                nome_Responsavel=nome_res,
+                nome_Paciente=nome,
+                cpf_responsavel=cpf,
+                data_Nascimento=data_nascimento,
+                email=email,
+                endereco=endereco,
+                telefone=telefone,
+                genero=genero,
+                idade_Paciente=idade_calculada,
+            )
+            paciente_instance.save()
+
+            # Mensagem de sucesso
+            messages.success(request, "Cadastro realizado com sucesso!")
+
+        except ValidationError as e:
+            # Mensagem de erro
+            messages.error(request, str(e))  # Passando a exceção como mensagem de erro
+
+    return render(request, '_layouts/consulta.html')
 
 
 
@@ -63,24 +111,3 @@ def consulta_view(request):
 
 
 
-
-
-#  def clean(self):
-#         # Valida o CPF do voluntário
-#         if len(self.cpf_Voluntario) != 11 or not self.cpf_Voluntario.isdigit():
-#             raise ValidationError("CPF do voluntário deve ter 11 dígitos numéricos.")
-        
-#         # Ou, se preferir usar a biblioteca CPF:
-#         # cpf = CPF()
-#         # if not cpf.validate(self.cpf_Voluntario):
-#         #     raise ValidationError("CPF do voluntário inválido.")
-        
-#         # Valida a idade do voluntário
-#         if self.data_Nascimento:
-#             today = date.today()
-#             idade_calculada = today.year - self.data_Nascimento.year - ((today.month, today.day) < (self.data_Nascimento.month, self.data_Nascimento.day))
-            
-#             if idade_calculada < 0:
-#                 raise ValidationError("Idade inválida calculada para o voluntário.")
-            
-#             self.idade_Voluntario = idade_calculada
